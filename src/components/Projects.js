1f5ac97110f6858e17fd4adad88e349e4ff64f28
bhyri-ajjay5 <bhyriajaykumar55@gmail.com>
2023-10-21 02:31:21 +0530

    added hover images in gallery route

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,79 @@
 import React from 'react'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { Card, CardMedia, Container,Grid, Typography } from '@mui/material';
+import '../css/Project.css'
 
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+  },
+];
 const Projects = () => {
   return (
-    <div>
-      <h1>Project</h1>
-    </div>
+    <>
+    <Container>
+      <Grid container spacing={2}>
+          {itemData.map((item) => (
+            <Grid item xs={12} md={4}>
+             <Card className="card">
+              <CardMedia className="media" 
+              image={item.img}
+              title={item.title} />
+              <div className="overlay">
+                <Typography variant="h6">{item.title}</Typography>
+              </div>
+            </Card>
+            </Grid>
+          ))}
+      </Grid>
+    </Container>
+    </>
   )
 }
 
